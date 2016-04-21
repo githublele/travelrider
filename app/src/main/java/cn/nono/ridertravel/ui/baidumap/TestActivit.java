03969be4be5496acdb7e6054abafd225881ebad8
githublele <1037111309@qq.com>
2016-04-21 13:36:33 +0800

    delte

--- app/src/main/java/cn/nono/ridertravel/ui/baidumap/TestActivit.java
+++
@@ -1,9 +0,0 @@
-package cn.nono.ridertravel.ui.baidumap;
-
-import cn.nono.ridertravel.ui.base.BaseNoTitleActivity;
-
-/**
- * Created by Administrator on 2016/4/21.
- */
-public class TestActivit extends BaseNoTitleActivity{
-}
