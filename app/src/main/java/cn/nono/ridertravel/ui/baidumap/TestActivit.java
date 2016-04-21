6d8a4df1728958ef393e2221c04bb6dfd03a06a2
githublele <1037111309@qq.com>
2016-04-21 13:34:46 +0800

    add and xiu gai

---
+++ app/src/main/java/cn/nono/ridertravel/ui/baidumap/TestActivit.java
@@ -0,0 +1,9 @@
+package cn.nono.ridertravel.ui.baidumap;
+
+import cn.nono.ridertravel.ui.base.BaseNoTitleActivity;
+
+/**
+ * Created by Administrator on 2016/4/21.
+ */
+public class TestActivit extends BaseNoTitleActivity{
+}
